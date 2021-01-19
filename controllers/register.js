87d1f0c39fc41611defda788e89f7e031ef9776f
StellaLulu <cty0220@gmail.com>
2021-01-19 16:57:39 +0800

    done Code Review and created controllers

---
+++ controllers/register.js
@@ -0,0 +1,34 @@
+// register - POST -> user
+// Use transaction in insert into login table
+// then loginEmail to insert into users table 
+const handleRegister = (req,res,db,bcrypt) => {
+    const {email, name, password} = req.body;
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('unable to register'))
+}
+
+module.exports = {
+    handleRegister: handleRegister
+};
