d51566c62a9430a12a2760fd992e4e4f843188ad
StellaLulu <cty0220@gmail.com>
2021-01-26 16:52:01 +0800

    changing response

--- server.js
+++ server.js
@@ -6,32 +6,81 @@ const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
 const db = knex({
-  // connect to your own database here
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '',
+      database : 'face-detection'
+    }
 });
+
+// Simple check of select * from a table
+// db.select('*').from('users').then(data=>{
+//     console.log(data);
+// });
 
 const app = express();
 
-app.use(cors())
+
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res)=> { res.send('It is working!') })
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+/*
+res = this is working
+/signin: POST = success /fail
+/register: POST = user
+/profile/:userId: GET = user
+/image: PUT user
+*/
+
+// there will be error if running... in heroku
+//app.get('/', (req, res)=> { res.send(database.users)})
+app.get('/', (req, res)=> { res.send('it is working!')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.post('/imageurl',(req,res) => { image.handleApiCall(req,res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('app is running');
 })
+
