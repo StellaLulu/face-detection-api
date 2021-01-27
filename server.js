66a35b1c0cfcce2c0a5f707fa900e72b9a14d5e0
StellaLulu <cty0220@gmail.com>
2021-01-27 17:01:52 +0800

    Reverted

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl : true,
+      ssl : true,
   }
 });
 
@@ -31,6 +31,6 @@ app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfile
 app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
+app.listen(process.env.PORT || 3000,  ()=> {
   console.log('app is running on port 3000');
 })
