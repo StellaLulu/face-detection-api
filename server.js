534715f4b4f0a4c7c89e82691e9372892c4ad94f
StellaLulu <cty0220@gmail.com>
2021-01-27 17:57:29 +0800

    testing

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.post('/imageurl',(req,res) => { image.handleApiCall(req,res)})
 //     // res = false
 // });
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`Our app is running on port ${ PORT }`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+    console.log(`Our app is running on port ${ PORT}`);
 })
